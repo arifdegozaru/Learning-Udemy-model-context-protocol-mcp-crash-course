0862f03b35c584ebc1066b00c6c98fb84c1dc665
ulik <arifdegozaru24@gmail.com>
2025-09-16 08:47:28 +0700

    feat: mcp server example: running on sse & stdio

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import asyncio
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from dotenv import load_dotenv
 
+load_dotenv()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def main():
+    print(os.getenv("OPEN_API_KEY"))
+    print("Hello World")
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
